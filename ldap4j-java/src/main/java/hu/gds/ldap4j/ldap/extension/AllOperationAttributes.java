f24384c27c505522ee18df5da78c87f156463f51
Directory Donkey <y@y.y>
2024-08-19 17:39:02 +0200

    add entryDN and entryUUID operational attributes

--- ldap4j-java/src/main/java/hu/gds/ldap4j/ldap/extension/AllOperationAttributes.java
+++ ldap4j-java/src/main/java/hu/gds/ldap4j/ldap/extension/AllOperationAttributes.java
@@ -7,6 +7,14 @@ import org.jetbrains.annotations.NotNull;
  */
 public class AllOperationAttributes {
     public static final @NotNull String ALL_OPERATIONAL_ATTRIBUTES="+";
+    /**
+     * RFC 5020
+     */
+    public static final @NotNull String ENTRY_DN="entryDN";
+    /**
+     * RFC 4530
+     */
+    public static final @NotNull String ENTRY_UUID="entryUUID";
     public static final @NotNull String FEATURE_OID="1.3.6.1.4.1.4203.1.5.1";
 
     private AllOperationAttributes() {
